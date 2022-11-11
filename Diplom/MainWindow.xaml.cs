a3e3a4ee015cede0be42cffe579f7790d2a3057a
LightingWizzard <LightingWizzard@outlook.com>
2022-11-11 20:12:45 +0900

    Added main functional: three methods of creating query for identifying ore type https://documentation.alastri.com/wiki/spaces/2MADS/pages/29823024/RR.+Reserves+Model+build+example#Editing-Reserves-Model

--- Diplom/MainWindow.xaml.cs
+++ Diplom/MainWindow.xaml.cs
@@ -21,11 +21,14 @@ namespace Diplom
     /// </summary>
     public partial class MainWindow : Window
     {
+        private byte methodnum = 1;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        //Next three methods are for hiding and showing different input fields
+        ///based on which button user clicks = which method they need
+        ////made realy sloopy but so what? It works :p
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             firstMethod.Visibility = Visibility.Visible;
@@ -34,6 +37,7 @@ namespace Diplom
             BtnM1.BorderBrush = new SolidColorBrush(Colors.LightSkyBlue);
             BtnM2.BorderBrush = new SolidColorBrush(Colors.Gray);
             BtnM3.BorderBrush = new SolidColorBrush(Colors.Gray);
+            methodnum = 1;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -44,6 +48,7 @@ namespace Diplom
             BtnM1.BorderBrush = new SolidColorBrush(Colors.Gray);
             BtnM2.BorderBrush = new SolidColorBrush(Colors.LightSkyBlue);
             BtnM3.BorderBrush = new SolidColorBrush(Colors.Gray);
+            methodnum = 2;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -54,6 +59,50 @@ namespace Diplom
             BtnM1.BorderBrush = new SolidColorBrush(Colors.Gray);
             BtnM2.BorderBrush = new SolidColorBrush(Colors.Gray);
             BtnM3.BorderBrush = new SolidColorBrush(Colors.LightSkyBlue);
+            methodnum = 3;
         }
+        //To decide which method we make
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            switch (methodnum)
+            {
+                case 1:
+                    resultQuery.Text = firstM();
+                    break;
+                case 2:
+                    resultQuery.Text = secondM();
+                    break;
+                case 3:
+                    resultQuery.Text = thirdM();
+                    break;
+                default:
+                    break;
+            }
+        }
+        //Methods realisation 
+        private string firstM()
+        {
+            string query = "N(\"" + oreNameM1.Text + "\") > " + oreValueM1.Text + " ? \"" + firstResultM1.Text + "\" : \"" + secondResultM1.Text + "\"";
+            return query;
+        }
+        private string secondM()
+        {
+            string query = "N(\"" + oreNameM2.Text + "\") > " + oreValue1M2.Text + " ? \"" + oreResult1M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue2M2.Text + " ? \"" + oreResult2M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue3M2.Text + " ? \"" + oreResult3M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue4M2.Text + " ? \"" + oreResult4M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue5M2.Text + " ? \"" + oreResult5M2.Text + "\" : \"" + oreResult6M2.Text + "\"";
+            return query;
+        }
+        private string thirdM()
+        {
+            string q3 = "N(\"" + oreAdd2NameM3.Text + "\") > " + oreAdd2ValueM3.Text + " ? " + "\"" + oreAddResult2M3.Text + "\" : \"" + oreAddResult1M3.Text + "\"";
+            string q2 = "N(\"" + oreAdd2NameM3.Text + "\") > " + oreAdd2ValueM3.Text + " ? " + "\"" + oreAddResult4M3.Text + "\" : \"" + oreAddResult3M3.Text + "\"";
+            string q1 = "N(\""+oreAdd1NameM3.Text+ "\") > "+oreAdd1ValueM3.Text+" ? "+(q2)+" : "+(q3);
+            string query = "N(\"" + oreMainNameM3.Text + "\") > " + oreValue1M3.Text + " ? " +(q1)+" : " + "N(\"" + oreMainNameM3.Text + "\") > " + oreValue2M3.Text + " ? \"" + oreResult2M3.Text + "\" : " + "N(\"" + oreMainNameM3.Text + "\") > " + oreValue3M3.Text + " ? \"" + oreResult3M3.Text + "\" : " + "N(\"" + oreMainNameM3.Text + "\") > " + oreValue4M3.Text + " ? \"" + oreResult4M3.Text + "\" : \""+oreResult5M3.Text + "\"";
+            return query;
+        }
+        /*
+private void TextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+{
+   TextBox txtBx = sender as TextBox;//This looks like it will be needed, so even though code is not working I leave it for the time being
+   txtBx.Text = "";
+}*/
     }
 }
