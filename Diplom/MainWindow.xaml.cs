a63dcdf1e8f9d26cd500e498e6afc71134e7336f
LightingWizzard <LightingWizzard@outlook.com>
2022-12-16 21:42:00 +0900

    Changed Method 1&2 (+change condition for them)

--- Diplom/MainWindow.xaml.cs
+++ Diplom/MainWindow.xaml.cs
@@ -25,7 +25,44 @@ namespace Diplom
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+        //This method will return symbol instead of string for comparison
+        private string ConditionStringToSymbol(string textValue)
+        {
+
+            switch (textValue)
+            {
+                case "больше":
+                    return ">";
+                    break;
+                case "больше или равно":
+                    return ">=";
+                    break;
+                case "меньше":
+                    return "<";
+                    break;
+                case "меньше или равно":
+                    return "<=";
+                    break;
+                case "равно":
+                    return "==";
+                    break;
+                case "неравное":
+                    return "!=";
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+            //return null;
+        }
+        //This method will cook an expression ala valueOne[oneOfSymbols:'>','>=','<','<=','==','!=']valueTwo?(resultWhenTrue):(resultWhenFalse)
+        private string makeAnExpression(string val1, string val2, string textValue, string resTrue, string resFalse)
+        {
+            return val1 + " " + ConditionStringToSymbol(textValue) + " " + val2 + " ? (" + resTrue + ") : (" + resFalse + ")";
+        }
+
         //Next three methods are for hiding and showing different input fields
         ///based on which button user clicks = which method they need
         ////made realy sloopy but so what? It works :p
@@ -78,17 +115,70 @@ namespace Diplom
                 default:
                     break;
             }
+            
         }
         //Methods realisation 
         private string firstM()
         {
-            string parcel = "N(\"" + oreNameM1.Text + "\") > " + oreValueM1.Text + " ? \"" + firstResultM1.Text + "\" : \"" + secondResultM1.Text + "\"";
-            return parcel;
+            //string parcel = "N(\"" + oreNameM1.Text + "\") > " + oreValueM1.Text + " ? \"" + firstResultM1.Text + "\" : \"" + secondResultM1.Text + "\"";
+            
+            try
+            {
+                string selectedListBoxItem_TextValue = ((ListBoxItem)MethodOneList.SelectedItem).Content.ToString();
+                string valueOne = "N(\"" + oreNameM1.Text + "\")";
+                string valueTwo = oreValueM1.Text;
+                string resultTrue = "\"" + firstResultM1.Text + "\"";
+                string resultFalse = "\"" + secondResultM1.Text + "\"";
+                string parcel = makeAnExpression(valueOne, valueTwo, selectedListBoxItem_TextValue, resultTrue, resultFalse);
+                return parcel;
+            }
+            catch (NullReferenceException e)
+            {
+                return "Error "+ "\"NullReferenceException\"" + "!\nSelect Condition before start!!!";
+            }
         }
         private string secondM()
         {
-            string parcel = "N(\"" + oreNameM2.Text + "\") > " + oreValue1M2.Text + " ? \"" + oreResult1M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue2M2.Text + " ? \"" + oreResult2M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue3M2.Text + " ? \"" + oreResult3M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue4M2.Text + " ? \"" + oreResult4M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue5M2.Text + " ? \"" + oreResult5M2.Text + "\" : \"" + oreResult6M2.Text + "\"";
-            return parcel;
+            //string parcel = "N(\"" + oreNameM2.Text + "\") > " + oreValue1M2.Text + " ? \"" + oreResult1M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue2M2.Text + " ? \"" + oreResult2M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue3M2.Text + " ? \"" + oreResult3M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue4M2.Text + " ? \"" + oreResult4M2.Text + "\" : " + "N(\"" + oreNameM2.Text + "\") > " + oreValue5M2.Text + " ? \"" + oreResult5M2.Text + "\" : \"" + oreResult6M2.Text + "\"";
+            try
+            {
+                //int[] array3 = { 1, 2, 3, 4, 5, 6 };
+                string[] selectedListBoxItem_TextValues = { 
+                    ((ListBoxItem)MTL1.SelectedItem).Content.ToString(),
+                    ((ListBoxItem)MTL2.SelectedItem).Content.ToString(),
+                    ((ListBoxItem)MTL3.SelectedItem).Content.ToString(),
+                    ((ListBoxItem)MTL4.SelectedItem).Content.ToString(),
+                    ((ListBoxItem)MTL5.SelectedItem).Content.ToString()
+                };
+                string valueOne = "N(\"" + oreNameM2.Text + "\")";
+                string[] valuesTwo = {
+                    oreValue1M2.Text,//0
+                    oreValue2M2.Text,//1
+                    oreValue3M2.Text,//2
+                    oreValue4M2.Text,//3
+                    oreValue5M2.Text,//4
+                };
+                string[] results =
+                {
+                    oreResult1M2.Text,//0
+                    oreResult2M2.Text,//1
+                    oreResult3M2.Text,//2
+                    oreResult4M2.Text,//3
+                    oreResult5M2.Text,//4
+                    oreResult6M2.Text,//5
+                };
+                string stroka = makeAnExpression(valueOne, valuesTwo[4], selectedListBoxItem_TextValues[4],results[4], results[5]);
+                for (int i=valuesTwo.Length-2; i>-1; i--)
+                {
+                    stroka = makeAnExpression(valueOne, valuesTwo[i], selectedListBoxItem_TextValues[i], results[i], stroka);
+                }
+                //stroka = makeAnExpression(valueOne, valuesTwo[0])
+                return stroka;
+            }
+            catch (NullReferenceException e)
+            {
+                return "Error " + "\"NullReferenceException\"" + "!\nSelect ALL Conditions before start!!!";
+            }
         }
         private string thirdM()
         {
